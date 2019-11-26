3fee6ea5575f901076bd555cd47fe47aad72522e
yuiyui83 <yui2056@yahoo.co.jp>
2019-11-26 16:08:14 +0900

    Initial Commit

---
+++ BLEComuClientForESPIDFble_spp_server/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLEComuClientForESPIDFble_spp_server
+//
+//  Created by 大塚悠一 on 2019/11/26.
+//  Copyright © 2019 YuichiOtsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
